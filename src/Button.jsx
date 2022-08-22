2fea5e97758ee3377a2199648586616194367a89
MIINII <ft.dawn.o@gmail.com>
2022-08-22 14:31:58 +0900

    인라인스타일로 적용해보기

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const Button = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+// 공통 버튼 스타일
+const baseButtonStyle = {
+  padding: '14px 27px',
+  outline: 'none',
+  cursor: 'pointer',
+  borderRadius: '9999px',
+  fontSize: '17px',
+};
+
+const blueButtonStyle = {
+  ...baseButtonStyle, // spread문법
+  border: 'solid 1px #7090ff',
+  color: '#7090ff',
+  backgroundColor: 'rgba(0, 89, 255, 0.2)',
+};
+
+const redButtonStyle = {
+  ...baseButtonStyle, // spread 문법
+  border: 'solid 1px #ff4664',
+  color: '#ff4664',
+  backgroundColor: 'rgba(255, 78, 78, 0.2)',
+};
+
+const Button = ({ children, onClick, color }) => {
+  const style = color === 'red' ? redButtonStyle : blueButtonStyle
+  return (
+    <button style={style} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
