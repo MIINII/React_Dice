15ed38135805c04972daf63cd9baef815af39f45
MIINII <ft.dawn.o@gmail.com>
2022-08-22 09:39:35 +0900

    컴포넌트 깔끔하게 정리하기

--- src/App.js
+++ src/App.js
@@ -12,37 +12,31 @@ function App() {
   // state를 사용할때는 변수에 새로운 값을 할당하면서 값을 변경하는게 아니라
   // setter함수를 통해서만 변경할수있다
 
-  // ##########################################################################################
-  // ## State Lifting : 자식컴포넌트의 State를 부모 컴포넌트로 올려주는것(원래 Board.jsx에 있던것들)  ##
-  // ##########################################################################################
+  // ###############################################################################################
+  // ## State Lifting : 자식컴포넌트의 State를 부모 컴포넌트로 올려주는것(원래 Board.jsx에 있던것들)       ##
+  // ###############################################################################################
 
   // 내꺼
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0);
-  const [gameHistory, setGameHistory] = useState([]);
+  // const [num, setNum] = useState(1);
+  // const [sum, setSum] = useState(0);
+  const [myHistory, setMyHistory] = useState([]);
 
   // 다른애꺼
-  const [OtherNum, setOtherNum] = useState(1);
-  const [otherSum, setOtherSum] = useState(0);
-  const [otherGameHistory, setOtherGameHistory] = useState([]);
+  // const [otherNum, setOtherNum] = useState(1);
+  // const [otherSum, setOtherSum] = useState(0);
+  const [otherHistory, setOtherHistory] = useState([]);
 
   // 이벤트 핸들러
   const handleRollClick = () => {
-    const nextNum = random(6);
+    const nextMyNum = random(6);
     const nextOtherNum = random(6);
-    setNum(nextNum);
-    setSum(nextNum);
-    
-    setGameHistory([...gameHistory, nextNum]); // spread
-    setOtherNum(nextNum);
-    setOtherSum(nextNum);
-    setOtherGameHistory([...gameHistory, nextNum]);
+    setMyHistory([...myHistory, nextMyNum]); // spread
+    setOtherHistory([...otherHistory, nextOtherNum]); // spread
   };
 
   const handleClearClick = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
+    setMyHistory([]);
+    setOtherHistory([]);
   };
 
   return (
@@ -54,8 +48,8 @@ function App() {
       </div>
 
       <div>
-        <Board name='나' color='blue' />
-        <Board name='상대' color='red' />
+        <Board name='나' color='blue' gameHistory={myHistory} />
+        <Board name='상대' color='red' gameHistory={otherHistory} />
       </div>
     </>
   );
